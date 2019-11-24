047bbba31f99643403efc0b72453b09fa696f2e1
davidmcclendon <davidmmcclendon@gmail.com>
2019-11-24 15:09:38 -0600

    First draft of map

---
+++ R/1_make_data.R
@@ -0,0 +1,82 @@
+library(tidyverse)
+library(here)
+library(janitor)
+library(readxl)
+library(sf)
+library(geojsonio)
+library(rmapshaper)
+library(sp)
+
+wgs84<-CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+
+#Election data
+df <- readxl::read_excel(here::here("data", "Updated Precinct Voting Data for Harris County.xlsx")) %>% 
+  dplyr::select(-c(VAP_Anglo:`...13`)) %>% 
+  mutate(
+    DB_Dem = ifelse(DB_Dem==".", NA, as.numeric(DB_Dem)),
+    DB_Rep = ifelse(DB_Rep==".", NA, as.numeric(DB_Rep)),
+    `Sen/Pre_Dem` = ifelse(`Sen/Pre_Dem`==".", NA, as.numeric(`Sen/Pre_Dem`)),
+    `Sen/Pre_Rep` = ifelse(`Sen/Pre_Rep`==".", NA, as.numeric(`Sen/Pre_Rep`)),
+    DB_Dem_pct = DB_Dem/(DB_Dem+DB_Rep)*100,
+    DB_Rep_pct = DB_Rep/(DB_Dem+DB_Rep)*100,
+    `Sen/Pre_Dem_pct` = `Sen/Pre_Dem`/(`Sen/Pre_Dem`+`Sen/Pre_Rep`)*100,
+    `Sen/Pre_Rep_pct` = `Sen/Pre_Rep`/(`Sen/Pre_Dem`+`Sen/Pre_Rep`)*100,
+    
+    db_diff = DB_Dem_pct - DB_Rep_pct,
+    sp_diff = `Sen/Pre_Dem_pct` - `Sen/Pre_Rep_pct`,
+    #year_array = (Year - 2000)/2,
+    
+    db_description = case_when(
+      db_diff>0 ~ paste0("Democrats +", round(abs(db_diff)), " percentage points"),
+      db_diff<0 ~ paste0("Republicans +", round(abs(db_diff)), " percentage points"),
+      db_diff==0 ~ paste0("Equal numbers of Democrats and Republicans")
+    ),
+    
+    sp_description = case_when(
+      sp_diff>0 ~ paste0("Democrats +", round(abs(sp_diff)), " percentage points"),
+      sp_diff<0 ~ paste0("Republicans +", round(abs(sp_diff)), " percentage points"),
+      sp_diff==0 ~ paste0("Equal numbers of Democrats and Republicans")
+    )
+  ) %>% 
+  dplyr::select(Pct, Year, db_diff, sp_diff, db_description, sp_description) %>% 
+  gather(election, values, db_diff:sp_description) %>% 
+  separate(election, into=c("election", "name"), sep="_") %>% 
+  filter(!is.na(values) & Year!="2020") %>% 
+  spread(name, values) %>% 
+  mutate(
+    diff=as.numeric(diff)
+    ) %>% 
+  arrange(Pct, election, Year) %>% 
+  group_by(Pct, election) %>% 
+  mutate(
+    year_array = (Year - 2000)/2,
+    #Different for presidential years
+    year_array = case_when(
+      Year==2000 & election=="sp" ~ 0,
+      Year==2004 & election=="sp" ~ 1,
+      Year==2008 & election=="sp" ~ 2,
+      Year==2012 & election=="sp" ~ 3,
+      Year==2016 & election=="sp" ~ 4,
+      Year==2018 & election=="sp" ~ 5,
+      TRUE ~ year_array
+    )
+  )
+  #filter(!is.na(year_array) & Year!=2020)
+  
+
+#Precinct shapefile
+sf <- read_sf(here::here("data", "Harris_County_Voting_Precincts.shp")) %>% 
+  st_as_sf() %>% st_transform(., wgs84) %>% 
+  mutate(Pct = as.numeric(PRECINCT)) %>% 
+  dplyr::select(Pct, geometry)
+
+#Merge
+combined <- left_join(sf, df, by="Pct") %>% 
+  geojson_json(.) %>% ms_simplify(.) 
+
+geojson_write(combined, file = here::here("data", paste0("mapdata", ".json")))
+geojson_write(combined, file = here::here(paste0("mapdata", ".json")))
+
+
+
+
